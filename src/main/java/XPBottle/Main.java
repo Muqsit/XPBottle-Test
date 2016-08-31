206d8a8400c199f41753d9812052d41a15a9afa6
Muqsit Rayyan <hackhack05@gmail.com>
2016-08-31 16:26:22 +0300

    Update Main.java

--- src/main/java/XPBottle/Main.java
+++ src/main/java/XPBottle/Main.java
@@ -1,5 +1,21 @@
 package XPBottle;
 
+/*
+*
+*  _    _ ______ ______                 _       
+* \ \  / (_____ (____  \       _   _   | |      
+*  \ \/ / _____) )___)  ) ___ | |_| |_ | | ____ 
+*   )  ( |  ____/  __  ( / _ \|  _)  _)| |/ _  )
+*  / /\ \| |    | |__)  ) |_| | |_| |__| ( (/ / 
+* /_/  \_\_|    |______/ \___/ \___)___)_|\____)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+*/
+
 import cn.nukkit.event.player.PlayerInteractEvent;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
